d2280f998f5186d8b1f2f4201bcdd99ecd2ce724
Deepesh <deepeshgodwani28@gmail.com>
2023-03-29 20:36:44 +0530

    created different branch for production

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = 7000;
 const cors = require("cors");
 const userAccount = require("./router/userAccountRoute");
 const userUpdate=require('./router/userUpdate');
+const path=require('path');
 
 connectDb();
 
@@ -14,6 +15,30 @@ app.use(express.json());
 
 app.use("/api/userAccount",userAccount );
 app.use("/api/userUpdate", userUpdate);
+
+
+
+
+// ------------------------Deployment---------------
+
+const __dirname1 = path.resolve();
+ 
+
+app.use(express.static(path.join(__dirname1,'frontend','build')))
+
+app.get('/',(req,res)=>{
+  res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
+});
+
+
+app.all("*", (req, res) => {
+  res.status(404).send("404 NOT FOUND");
+});
+
+
+
+
+
 
 app.listen(PORT, (err) => {
   if (err) {
